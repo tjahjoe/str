902dac679db79ed9e43d2f513ba5da214c3bddc0
tjahjoe <ntell634@gmail.com>
2025-04-12 17:37:11 +0700

    add: chromadb

--- chat.py
+++ chat.py
@@ -1,10 +1,58 @@
+# import streamlit as st
+# from io import StringIO
+# from model_genai import ModelGenai
+
+# class ChatPage:
+#     def __init__(self):
+#         self.__model = ModelGenai()
+
+#     # @st.cache_resource
+#     # def __load_model(_self):
+#     #     return ModelGenai()
+
+#     # @st.cache_resource
+#     # def __load_chroma(_self):
+#     #     return ChromaDatabase()
+
+#     def show(self):
+#         st.title('Asisten Ahli Tanaman 🌿')
+#         st.markdown('Tanyakan apa saja tentang tanaman. Aku tidak bisa mengingat pertanyaan sebelumnya.')
+
+#         question = st.text_input('Pertanyaan Anda:')
+
+#         if st.button('Tanya') and question.strip():
+#             self.__handle_question(question)
+#         else:
+#             st.info("Masukkan pertanyaan lalu klik tombol 'Tanya'.")
+
+#     def __handle_question(self, question):
+#         with st.spinner('Menjawab...'):
+#             try:
+#                 full_answer = StringIO()
+#                 answer_stream = self.__model.chain.stream({'input': question})
+
+#                 response_container = st.empty()
+#                 output = ''
+
+#                 for chunk in answer_stream:
+#                     content = getattr(chunk, 'content', str(chunk))
+#                     output += content
+#                     response_container.markdown(output + '▌') 
+
+#                 response_container.markdown(output) 
+#                 st.success('✅ Jawaban disimpan ke database.')
+#             except Exception as e:
+#                 st.error('❌ Terjadi kesalahan. Mungkin kuota API habis.', e)
+
+
 import streamlit as st
-from io import StringIO
 from model_genai import ModelGenai
+from chroma_database import ChromaDatabase
 
 class ChatPage:
     def __init__(self):
         self.__model = ModelGenai()
+        self.__chroma_database = ChromaDatabase()
 
     # @st.cache_resource
     # def __load_model(_self):
@@ -16,7 +64,7 @@ class ChatPage:
 
     def show(self):
         st.title('Asisten Ahli Tanaman 🌿')
-        st.markdown('Tanyakan apa saja tentang tanaman. Aku tidak bisa mengingat pertanyaan sebelumnya.')
+        st.markdown('Tanyakan apa saja tentang tanaman. Untuk pertanyaan non-tanaman akan ditolak secara sopan.')
 
         question = st.text_input('Pertanyaan Anda:')
 
@@ -28,8 +76,7 @@ class ChatPage:
     def __handle_question(self, question):
         with st.spinner('Menjawab...'):
             try:
-                full_answer = StringIO()
-                answer_stream = self.__model.chain.stream({'input': question})
+                answer_stream = self.__model.chain.stream({'question': question})
 
                 response_container = st.empty()
                 output = ''
@@ -40,8 +87,10 @@ class ChatPage:
                     response_container.markdown(output + '▌') 
 
                 response_container.markdown(output) 
+
+                self.__chroma_database.insert_qa_to_chroma(output, question)
                 st.success('✅ Jawaban disimpan ke database.')
             except Exception as e:
-                st.error('❌ Terjadi kesalahan. Mungkin kuota API habis.', e)
+                st.error('❌ Terjadi kesalahan. Mungkin kuota API habis.')
 
 
---
+++ chroma_database.py
@@ -0,0 +1,40 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.docstore.document import Document
+
+class ChromaDatabase():
+    def __init__(self):
+        self.__GOOGLE_API_KEY = 'AIzaSyCIRPP0PhNPAR4JNiHxad-vOk6jk7JhCBM'
+
+        self.__model_embedding = 'models/embedding-001'
+        self.__collection_name = 'my_collection'
+        self.__persist_directory = './chroma'
+
+        self.__first_text = '''Kamu adalah ahli tanaman dan juga teman saya.'''
+
+        self.__embedding = GoogleGenerativeAIEmbeddings(
+            google_api_key=self.__GOOGLE_API_KEY,
+            model=self.__model_embedding
+        )
+
+        self.__vectorstore = Chroma(
+            collection_name=self.__collection_name,
+            embedding_function=self.__embedding,
+            persist_directory=self.__persist_directory
+        )
+
+        self.__insert_first_text_to_chroma()
+
+        self.retriever = self.__vectorstore.as_retriever()
+
+    def __insert_first_text_to_chroma(self):
+        if len(self.__vectorstore.get()['documents']) == 0:
+            splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=10)
+            docs = splitter.create_documents([self.__first_text])
+            self.__vectorstore.add_documents(docs)
+
+    def insert_qa_to_chroma(self, answer, question):
+        content = f"Q: {question}\nA: {answer}"
+        document = Document(page_content=content)
+        self.__vectorstore.add_documents([document])
